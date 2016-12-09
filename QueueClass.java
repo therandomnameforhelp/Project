5a51c33d7f807d2c31b8d4c8cc314628d5db1a34
therandomnameforhelp <keanucorrea@yahoo.com>
2016-12-08 14:34:36 -1000

    Add files via upload

---
+++ QueueClass.java
@@ -0,0 +1,57 @@
+/*
+Name:Queue
+Developer:Keanu Correa
+Date:12/01/2016
+*/
+
+public class QueueClass {
+   private static final int capacity = 10;
+   int arrayStack[] = new int[capacity];
+   int top = -1;
+   int rear = 0;
+
+   public void push(int pushedElement) {
+      if (top < capacity - 1) {
+      top++;
+      arrayStack[top] = pushedElement;
+      System.out.println(pushedElement+ " is pushed to Queue");
+      peekQue();
+      } 
+   }
+
+   public void isFull() {
+      if (top < capacity - 1) {;
+      }
+      else {
+      System.out.println("Queue Full");
+      }
+   }
+      
+   public void pop(int poppedElement) {
+      if (top >= rear) {
+      rear++;
+      System.out.println( poppedElement + " popped from Queue");
+      peekQue();
+      }
+   }
+   
+   public void isEmpty() {
+      if (top >= rear) {;
+      }
+      else {
+      System.out.println("Queue Empty");
+      }
+   }
+
+   public void peekQue() {
+      if (top >= rear) {
+      System.out.println("Queue: ");
+      for (int i = rear; i <= top; i++) {
+      System.out.print(arrayStack[i] + " ");
+      }
+      }
+      
+   }
+
+}
+
---
+++ StackClass.java
@@ -0,0 +1,57 @@
+/*
+Name:Stack
+Developer:Keanu Correa
+Date:12/01/2016
+*/
+
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+
+public class StackClass {
+   private static final int capacity = 10;
+   int arrayStack[] = new int[capacity];
+   int top = -1;
+   
+   public void push(int pushedInteger) {
+      if (top < capacity - 1) {
+      top++;
+      arrayStack[top] = pushedInteger;
+      System.out.println(pushedInteger + " is pushed to Stack");
+      peekStack();
+      }
+   }
+   
+   public void isFull() {
+      if (top < capacity - 1) {
+      ;
+      } 
+      else {
+      System.out.println("Stack Full");
+      }
+   }
+
+   public void pop(int poppedInt) {
+      if (top >= 0) {
+      top--;
+      System.out.println(poppedInt + " Popped from stack");
+      peekStack();
+      } 
+   }
+
+   public void isEmpty() {
+      if (top>= 0){ ;
+      }
+      else {
+      System.out.println("Stack Empty");
+      }
+   }
+ 
+   public void peekStack() {
+      System.out.println("Stack : ");
+      for (int i = 0; i <= top; i++) {
+      System.out.print(arrayStack[i] + " " );
+      }
+   }     
+}
